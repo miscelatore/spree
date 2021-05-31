792422897728f14d428e5b0d911c8ff5949c1311
Stefano Ruberti <stefano.ruberti@gmail.com>
2021-05-31 19:02:03 +0200

    Fix some minor change on sandbox.sh Create version script of sandbox_gem_from_github.sh to run deploy on VPS server

--- bin/sandbox_gem_from_github.sh
+++ bin/sandbox_gem_from_github.sh
@@ -38,57 +38,37 @@ fi
 
 cd ./sandbox
 
-if [ "$SPREE_HEADLESS" != "" ]; then
-  bundle config local.spree_core /Users/mixer/RoR/Rails6/spree
-  bundle config local.spree_api /Users/mixer/RoR/Rails6/spree
-  bundle config local.spree_backend /Users/mixer/RoR/Rails6/spree
-  bundle config local.spree_sample /Users/mixer/RoR/Rails6/spree
-  bundle config local.spree_cmd /Users/mixer/RoR/Rails6/spree
-else
-  bundle config local.spree /Users/mixer/RoR/Rails6/spree
-fi
-
-bundle config local.spree_i18n /Users/mixer/RoR/Rails6/spree_i18n
-bundle config local.spree_static_content /Users/mixer/RoR/Rails6/spree_static_content
-bundle config local.spree_related_products /Users/mixer/RoR/Rails6/spree_related_products
-# bundle config local.spree_multi_domain /Users/mixer/RoR/Rails6/spree-multi-domain
-# bundle config local.spree_mail_settings /Users/mixer/RoR/Rails6/spree_mail_settings
-
 if [ "$SPREE_AUTH_DEVISE_PATH" != "" ]; then
   SPREE_AUTH_DEVISE_GEM="gem 'spree_auth_devise', path: '$SPREE_AUTH_DEVISE_PATH'"
 else
-  bundle config local.spree_auth_devise /Users/mixer/RoR/Rails6/spree_auth_devise
-  SPREE_AUTH_DEVISE_GEM="gem 'spree_auth_devise', git: 'spree_auth_devise/spree_auth_devise', branch: 'spree_auth_devise_nx'"
+  SPREE_AUTH_DEVISE_GEM="gem 'spree_auth_devise', github: 'miscelatore/spree_auth_devise', branch: 'spree_auth_devise_nx'"
 fi
 
 if [ "$SPREE_GATEWAY_PATH" != "" ]; then
   SPREE_GATEWAY_GEM="gem 'spree_gateway', path: '$SPREE_GATEWAY_PATH'"
 else
-  bundle config local.spree_gateway /Users/mixer/RoR/Rails6/spree_gateway
-  SPREE_GATEWAY_GEM="gem 'spree_gateway', git: 'spree_gateway/spree_gateway', branch: 'spree_gateway_nx'"
+  SPREE_GATEWAY_GEM="gem 'spree_gateway', github: 'miscelatore/spree_gateway', branch: 'spree_gateway_nx'"
 fi
 
 if [ "$BETTER_SPREE_PAYPAL_EXPRESS_PATH" != "" ]; then
   BETTER_SPREE_PAYPAL_EXPRESS="gem 'spree_paypal_express', path: '$BETTER_SPREE_PAYPAL_EXPRESS_PATH'"
 else
-  bundle config local.spree_paypal_express /Users/mixer/RoR/Rails6/better_spree_paypal_express
-  BETTER_SPREE_PAYPAL_EXPRESS="gem 'spree_paypal_express', git: 'better_spree_paypal_express/better_spree_paypal_express', branch: 'better_spree_paypal_express_nx'"
+  BETTER_SPREE_PAYPAL_EXPRESS="gem 'spree_paypal_express', github: 'miscelatore/better_spree_paypal_express', branch: 'better_spree_paypal_express_nx'"
 fi
-
 
 if [ "$SPREE_HEADLESS" != "" ]; then
 cat <<RUBY >> Gemfile
-gem 'spree_core', git: 'spree_core/spree_core', branch: 'spree_core_nx'
-gem 'spree_api', git: 'spree_api/spree_api', branch: 'spree_api_nx'
-gem 'spree_backend', git: 'spree_backend/spree_backend', branch: 'spree_backend_nx'
-gem 'spree_sample', git: 'spree_sample/spree_sample', branch: 'spree_sample_nx'
-gem 'spree_cmd', git: 'spree_cmd/spree_cmd', branch: 'spree_cmd_nx'
+gem 'spree_core', github: 'miscelatore/spree_core', branch: 'spree_core_nx'
+gem 'spree_api', github: 'miscelatore/spree_api', branch: 'spree_api_nx'
+gem 'spree_backend', github: 'miscelatore/spree_backend', branch: 'spree_backend_nx'
+gem 'spree_sample', github: 'miscelatore/spree_sample', branch: 'spree_sample_nx'
+gem 'spree_cmd', github: 'miscelatore/spree_cmd', branch: 'spree_cmd_nx'
 
 $SPREE_AUTH_DEVISE_GEM
 $SPREE_GATEWAY_GEM
 $BETTER_SPREE_PAYPAL_EXPRESS
 
-gem 'spree_i18n', git: 'spree_i18n/spree_i18n', branch: 'spree_i18n_nx'
+gem 'spree_i18n', github: 'miscelatore/spree_i18n', branch: 'spree_i18n_nx'
 
 group :test, :development do
   gem 'bullet'
@@ -100,14 +80,15 @@ gem 'rack-cache'
 RUBY
 else
 cat <<RUBY >> Gemfile
-gem 'spree', git: 'spree/spree', branch: 'spree_nx'
+gem 'spree', github: 'miscelatore/spree_i18n', branch: 'spree_nx'
 $SPREE_AUTH_DEVISE_GEM
 $SPREE_GATEWAY_GEM
 $BETTER_SPREE_PAYPAL_EXPRESS
-gem 'spree_i18n', git: 'spree_i18n/spree_i18n', branch: 'spree_i18n_nx'
-gem 'spree_static_content', git: 'spree_static_content/spree_static_content', branch: 'spree_static_content_nx'
-gem 'spree_related_products', git: 'spree_related_products/spree_related_products', branch: 'spree_related_products_nx'
-# gem 'spree_multi_domain', git: 'spree-multi-domain/spree-multi-domain', branch: 'spree_multi_domain_nx'
+
+gem 'spree_i18n', github: 'miscelatore/spree_i18n', branch: 'spree_i18n_nx'
+gem 'spree_static_content', github: 'miscelatore/spree_static_content', branch: 'spree_static_content_nx'
+gem 'spree_related_products', github: 'miscelatore/spree_related_products', branch: 'spree_related_products_nx'
+# gem 'spree_multi_domain', github: 'miscelatore/spree-multi-domain', branch: 'spree_multi_domain_nx'
 
 group :test, :development do
   gem 'bullet'
