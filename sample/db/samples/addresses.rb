e76e61bfea8355e2fc95789e78345c3615a3d8ef
Stefano Ruberti <stefano.ruberti@gmail.com>
2021-06-29 15:37:40 +0200

    Change data of italian address

--- sample/db/samples/addresses.rb
+++ sample/db/samples/addresses.rb
@@ -1,17 +1,17 @@
-united_states = Spree::Country.find_by!(iso: 'US')
-new_york = Spree::State.find_by!(name: 'New York')
+italy = Spree::Country.find_by!(iso: 'IT')
+firenze = Spree::State.find_by!(name: 'Firenze')
 
 # Billing address
 Spree::Address.create!(
-  firstname: FFaker::Name.first_name,
-  lastname: FFaker::Name.last_name,
+  firstname: FFaker::NameIT.first_name,
+  lastname: FFaker::NameIT.last_name,
   address1: FFaker::Address.street_address,
   address2: FFaker::Address.secondary_address,
   city: FFaker::Address.city,
-  state: new_york,
-  zipcode: 16804,
-  country: united_states,
-  phone: FFaker::PhoneNumber.phone_number
+  state: firenze,
+  zipcode: 50100,
+  country: italy,
+  phone: FFaker::PhoneNumberIT.phone_number
 )
 
 # Shipping address
@@ -21,8 +21,8 @@ Spree::Address.create!(
   address1: FFaker::Address.street_address,
   address2: FFaker::Address.secondary_address,
   city: FFaker::Address.city,
-  state: new_york,
+  state: firenze,
   zipcode: 16804,
-  country: united_states,
-  phone: FFaker::PhoneNumber.phone_number
+  country: 50100,
+  phone: FFaker::PhoneNumberIT.phone_number
 )
