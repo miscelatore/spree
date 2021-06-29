8c8b3f4cb6ac419c94c0a0bf0c02e74d931b5444
Stefano Ruberti <stefano.ruberti@gmail.com>
2021-06-29 15:38:22 +0200

    Change payment methods

--- sample/db/samples/payment_methods.rb
+++ sample/db/samples/payment_methods.rb
@@ -1,17 +1,39 @@
-cc_payment_method = Spree::Gateway::Bogus.where(
-  name: 'Credit Card',
-  description: 'Bogus payment gateway.',
+# cc_payment_method = Spree::Gateway::Bogus.where(
+#   name: 'Credit Card',
+#   description: 'Bogus payment gateway.',
+#   active: true
+# ).first_or_create!
+#
+# cc_payment_method.store_ids = Spree::Store.ids
+# cc_payment_method.save!
+#
+# check_paymemt_method = Spree::PaymentMethod::Check.where(
+#   name: 'Check',
+#   description: 'Pay by check.',
+#   active: true
+# ).first_or_create!
+#
+# check_paymemt_method.store_ids = Spree::Store.ids
+# check_paymemt_method.save!
+
+cc_payment_method = Spree::Gateway::PayPalExpress.where(
+  name: 'PayPal',
+  description: 'Gateway di pagamento PayPal.',
+  display_on: 'both',
+  auto_capture: true,
   active: true
 ).first_or_create!
 
 cc_payment_method.store_ids = Spree::Store.ids
 cc_payment_method.save!
 
-check_paymemt_method = Spree::PaymentMethod::Check.where(
-  name: 'Check',
-  description: 'Pay by check.',
+cc_payment_method = Spree::Gateway::StripeGateway.where(
+  name: 'Stripe',
+  description: 'Gateway di pagamento Stripe.',
+  display_on: 'both',
+  auto_capture: true,
   active: true
 ).first_or_create!
 
-check_paymemt_method.store_ids = Spree::Store.ids
-check_paymemt_method.save!
+cc_payment_method.store_ids = Spree::Store.ids
+cc_payment_method.save!
